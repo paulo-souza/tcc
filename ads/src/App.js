9170381ef92ccdc4c5be053c7cdf9d90d75e720e
paulo-souza <phespecialmaster@gmail.com>
2021-05-21 15:20:44 -0300

    1 - Cria estrutura do projeto
    Para definir e organizar a estrutura de pastas do projeto.

---
+++ ads/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1> --- Hello World React JS --- </h1>
+      <h2> Teste h2 </h2>
+      <h3> Teste h3 </h3>
+
+      <p id='teste'>Fulando de tal tal tal</p>
+      <a href='#' title='link de teste'>Testando link</a>
+    </div>
+  );
+}
+
+
