90515307001de219ccd488d1c9f91f3eb990caf9
paulo-souza <phespecialmaster@gmail.com>
2021-05-22 15:52:39 -0300

    2 - Cria funcionalidade de login
    Para que o usuário possa acessar o sistema.

--- ads/src/App.js
+++ ads/src/App.js
@@ -1,16 +1,9 @@
-import './App.css';
+import React from 'react';
+import SignIn from './Page/SignIn';
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1> --- Hello World React JS --- </h1>
-      <h2> Teste h2 </h2>
-      <h3> Teste h3 </h3>
-
-      <p id='teste'>Fulando de tal tal tal</p>
-      <a href='#' title='link de teste'>Testando link</a>
-    </div>
-  );
+  return <SignIn />;
+ 
 }
 
 
---
+++ ads/src/Page/SignIn/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import '../../Css/SignIn.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
+
+export default function SignIn() {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const altura = window.innerHeight;
+    
+    const container = {
+        display: '-moz-flex',
+        display: '-webkit-flex',
+        display: '-ms-flex',
+        display: 'flex',
+        flexDirection: 'column',
+        height: `${altura}px`,  
+        backgroundColor: '#F5FFFA',
+        justifyContent: 'center',
+        alignItems: 'center'
+    };
+
+    
+
+    return (
+        <div style={container}>            
+            <div id={'signin_background'}>
+
+                <div className={'signin_container'}>
+                    <FontAwesomeIcon icon={faUserCircle} color={'#4F4F4F'} size={'6x'} />
+
+                    <h3 id={'signin_h3'}>Acessar sistema</h3>
+                </div>
+                
+                <hr className={'signin_hr'} />
+
+                <br />
+
+                <label>E-mail</label>
+                <input name={'email'} value={email} type={'email'} placeholder={'E-mail'}
+                    autoFocus={true} onChange={e => setEmail(e.target.value) } />
+
+                <label>Senha</label>
+                <input name={'senha'} value={senha} type={'password'}
+                    placeholder={'Senha'} onChange={e => setSenha(e.target.value)} />
+                
+                <button type={'button'} id={'signin_button'} onClick={()=> alert(`email: ${email}\nsenha: ${senha}`)}>Entrar</button>
+
+                <div className={'signin_container'}>                    
+                    <a href={'#'}>Esqueci minha senha</a>                    
+                </div>
+
+                <hr className={'signin_hr'}/>                
+                
+            </div>    
+              
+        </div>      
+    );
+}
