070b7d5eab6a84f7cb6be408876243c76b1776aa
paulo-souza <phespecialmaster@gmail.com>
2021-05-25 11:15:38 -0300

    9 - Refatora css de BtnVoltar
    Para o css Main, porque será utilizado em outros botões.

--- ads/src/Components/BtnVoltar/index.js
+++ ads/src/Components/BtnVoltar/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import '../../Css/BtnVoltar.css';
 
 export default function BtnVoltar({path}) {
     return( 
-        <Link to={path} className={'btnVoltar'}>
+        <Link to={path} className={'btnNav'}>
             <FontAwesomeIcon icon={faArrowLeft} color={'#000'} size={'lg'} />            
         </Link>
         );
