a19363090b6e502ae93efa9f4895c1e5d62bdb1b
paulo-souza <phespecialmaster@gmail.com>
2021-05-26 09:53:54 -0300

    14 - Refatora nav
    Para um novo componente chamado "Header" que possibilitará a reutilização em outras funcionalidades do sistema.

---
+++ ads/src/Components/Header/index.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../../Context/AuthProvider';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell, faUser } from '@fortawesome/free-solid-svg-icons';
+
+export default function Header() {
+    const { toogleModalUsuario } = useContext(AuthContext);
+
+    return (
+        <nav>
+            <div className={'container_title'}>
+                <h2 className={'titulo'}>Clientes</h2>
+            </div>
+
+            <div className={'container_buttons'}>
+                <button type={'button'} title={'Notificações'} className={'btnNav'}>
+                    <FontAwesomeIcon icon={faBell} color={'#DAA520'} size={'lg'} />
+                </button>
+
+                <button type={'button'} title={'Gerenciar conta de usuário'} className={'btnNav'} onClick={toogleModalUsuario}>
+                    <FontAwesomeIcon icon={faUser} color={'#4682B4'} size={'lg'} />
+                </button>
+            </div>
+        </nav>
+    );
+}
--- ads/src/Page/Cliente/index.js
+++ ads/src/Page/Cliente/index.js
@@ -1,31 +1,16 @@
 import React, { useContext } from 'react';
+import Header from '../../Components/Header';
 import Footer from '../../Components/Footer';
 import { AuthContext } from '../../Context/AuthProvider';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell, faUser } from '@fortawesome/free-solid-svg-icons';
 import ModalUsuario from '../../Components/Modal/Usuario';
 
 export default function Cliente() {
-    const { container, toogleModalUsuario, abrirModalUsuario } = useContext(AuthContext);
+    const { container, abrirModalUsuario } = useContext(AuthContext);
 
 
     return(
         <div style={container}>
-            <nav>
-                <div className={'container_title'}>
-                    <h2 className={'titulo'}>Clientes</h2>
-                </div>
-
-                <div className={'container_buttons'}>
-                    <button type={'button'} title={'Notificações'} className={'btnNav'}>
-                        <FontAwesomeIcon icon={faBell} color={'#DAA520'} size={'lg'} />  
-                    </button>
-
-                    <button type={'button'} title={'Gerenciar conta de usuário'} className={'btnNav'} onClick={toogleModalUsuario}>
-                        <FontAwesomeIcon icon={faUser} color={'#4682B4'} size={'lg'} />  
-                    </button>
-                </div>
-            </nav>
+            <Header />
 
             <div className={'body'}>
                 <h1>Aqui é o Body</h1>
