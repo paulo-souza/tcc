bd8e20a44ca181b0d9b0099dfcbec85eeb9efec1
paulo-souza <phespecialmaster@gmail.com>
2021-06-07 16:36:17 -0300

    37 - Adiciona rota de novo e editar PJ
    Para possibilitar a edição do cliente que é pessoa jurídica e criar um caso não exista.

--- ads/src/Routes/app.routes.js
+++ ads/src/Routes/app.routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Clientes from '../Page/Clientes';
 import EditarOuNovoCliente from '../Page/Clientes/EditarOuNovoCliente';
+import PessoaJuridica from '../Page/Clientes/PessoaJuridica';
 
 export default function AppRoutes() {
     return(
@@ -9,7 +10,9 @@ export default function AppRoutes() {
             <Switch>
                 <Route exact path={'/'} component={Clientes} />
                 <Route exact path={'/Clientes/Novo'} component={EditarOuNovoCliente} />
+                <Route exact path={'/Clientes/Novo/PessoaJuridica'} component={PessoaJuridica} />
                 <Route exact path={'/Clientes/Editar/:uid'} component={EditarOuNovoCliente} />
+                <Route exact path={'/Clientes/Editar/:uid/PessoaJuridica'} component={PessoaJuridica} />
             </Switch>
         </BrowserRouter>
 
