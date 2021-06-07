d4c463d00d36c06a22b088cc0f3cf5e9d23602a3
paulo-souza <phespecialmaster@gmail.com>
2021-06-07 18:04:31 -0300

    39 - Cria formulário de PJ
    Para criar ou editar um cliente.

--- ads/src/Components/PJMaisSocios/index.js
+++ ads/src/Components/PJMaisSocios/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 export default function PJMaisSocios(props) {
     return (
         <section id={'section1'}>
--- ads/src/Page/Clientes/PessoaJuridica/index.js
+++ ads/src/Page/Clientes/PessoaJuridica/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
+import '../../../Css/PessoaJuridica.css';
 
 export default function PessoaJuridica(props) {
     const {uid} = useParams();
@@ -14,6 +15,65 @@ export default function PessoaJuridica(props) {
                 <span className={'niveis separadorNiveis'}>{'>'}</span>
                 <span className={'niveis'}>Pessoa Jurídica</span>
             </div>
+
+            <fieldset className={'formulario'}>
+                <legend align={'center'} className={'formulario'}>Cliente</legend>
+                
+                <div>
+                    <label htmlFor={'situacao_empresa'}>Situação da empresa*</label>              
+                </div>
+                <select className={'pj'} name={'situacao_empresa'} id={'situacao_empresa'} value={''}>
+                    <option value={'ativo'}>Ativo</option>
+                    <option value={'inativo'}>Inativo</option>                    
+                </select>
+
+                <label htmlFor={'data_registro'}>Data registro*</label>
+                <input className={'pj'} id={'data_registro'} name={'data_registro'} value={''} type={'date'} />
+
+                <label htmlFor={'razao_social'}>Razão Social*</label>
+                <input className={'pj'} id={'razao_social'} name={'razao_social'} value={''}
+                    type={'text'} placeholder={'Razão Social'}  />
+
+                <label htmlFor={'nome_fantasia'}>Nome fantasia</label>
+                <input className={'pj'} id={'nome_fantasia'} name={'nome_fantasia'} value={''}
+                    type={'text'} placeholder={'Nome fantasia'}  />
+
+                <div>
+                    <label htmlFor={'natureza_juridica'}>Natureza jurídica*</label>
+                </div>
+                <select className={'pj'} name={'natureza_juridica'} id={'natureza_juridica'} value={''}>
+                    <option value={'Ltda'}>Ltda</option>
+                    <option value={'MEI'}>MEI</option>                    
+                    <option value={'EI'}>EI</option>                    
+                    <option value={'EIRELI'}>EIRELI</option>                    
+                </select>
+
+                <div>
+                    <label htmlFor={'porte_empresa'}>Porte da empresa*</label>
+                </div>
+                <select className={'pj'} name={'porte_empresa'} id={'porte_empresa'} value={''}>
+                    <option value={'ME'}>ME - Microempresa</option>
+                    <option value={'EPP'}>EPP - Empresa de Pequeno Porte</option>                  
+                </select>
+
+                <label htmlFor={'cnae'}>CNAE*</label>
+                <input className={'pj'} id={'cnae'} name={'cnae'} value={''}
+                    type={'text'} placeholder={'CNAE'}  />
+
+                <label htmlFor={'cnpj'}>CNPJ*</label>
+                <input className={'pj'} id={'cnpj'} name={'cnpj'} value={''}
+                    type={'text'} placeholder={'CNPJ'}  />
+
+                <label htmlFor={'inscricao_municipal'}>Inscrição Municipal*</label>
+                <input className={'pj'} id={'inscricao_municipal'} name={'inscricao_municipal'} value={''}
+                    type={'text'} placeholder={'Inscrição Municipal'}  />
+
+                <label htmlFor={'inscricao_estadual'}>Inscrição Estadual</label>
+                <input className={'pj'} id={'inscricao_estadual'} name={'inscricao_estadual'} value={''}
+                    type={'text'} placeholder={'Inscrição Estadual'}  />
+
+                <button className={'btnSalvarPJ'} type={'button'}>Salvar</button>
+            </fieldset>
         </div>
     );
 }
