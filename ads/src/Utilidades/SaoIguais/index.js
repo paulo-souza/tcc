dfc7ea8e7874b02604032f930b172564b5c103d7
paulo-souza <phespecialmaster@gmail.com>
2021-06-10 14:58:26 -0300

    45 - Cria função utilitária para comparação de objetos
    Para verificar se dois objetos são iguais.
    
    OBS.: Esta função não verifica objetos que possuem outros dentro de si.

---
+++ ads/src/Utilidades/SaoIguais/index.js
@@ -0,0 +1,13 @@
+
+// Não funciona se um objeto possuir outro dentro de si
+export default function SaoIguais(objetoA, objetoB) {
+
+    let aChaves = Object.keys(objetoA);
+    let bChaves = Object.keys(objetoB);
+
+    if (aChaves.length != bChaves.length) return false;
+  
+    let saoDiferentes = aChaves.some(chave => objetoA[chave] !== objetoB[chave]);
+
+    return !saoDiferentes;
+}
